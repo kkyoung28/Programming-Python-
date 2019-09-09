327c84a96dd29b86562ccd44d1977aa04f13a92e
Gayoung Kim <s2018w23@e-mirim.hs.kr>
2019-09-09 10:00:45 +0900

    add datetimemodule

---
+++ 패키지/datetimemodule.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+print('현제 날짜 시각 객체 얻기')
+today = datetime.now()
+print('today = datetime.now() : ', today)
+print('년 월 일 :  today.year, today.month, today.day',today.year, today.month, today.day)
+print('시 분 초 : today.hour, today.minute, today.second', today.hour, today.minute, today.second)
+print('요일 : ', today.weekday())
+day = datetime(2019, 1, 1, 0, 0, 0)
+print('day = datetime(2019, 1, 1, 0, 0, 0) : ', day)
+print('2019년부터 지나온 시간 구하기')
+print('today - day : ', today - day)
+
+#내가 태어난 날은 무슨 요일?
+birthday = datetime(1967, 5, 26, 9, 0, 0)
+print("월화수목금토일"[birthday.weekday()], "요일")
+#나는 며칠 살았을까?
+print(today - birthday)
+#올해 크리스마스 며칠 남았을까?
+christmas = datetime(2019,12,25,0,0,0)
+print(today - christmas)
