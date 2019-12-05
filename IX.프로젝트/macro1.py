cea3c90765b2176cebe3005cc8009e8ca0e1c8dc
Gayoung Kim <s2018w23@e-mirim.hs.kr>
2019-12-05 10:10:59 +0900

    pyautogui

---
+++ IX.프로젝트/macro1.py
@@ -0,0 +1,12 @@
+import pyautogui as pag
+
+if __name__ == '__main__':
+    pag.moveTo(113,534, duration=2)
+    # pag.click()
+    # pag.click()
+    # pag.click(clicks=2)
+    # pag.doubleClick()
+    # pag.move(100,200, duration=1)
+    # pag.rightClick()
+    # pag.click(button="right")
+    pag.drag(0,200,duration=2)
